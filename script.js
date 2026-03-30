44314b913f7d6ed6283339cf3933eb2cfa0c7f94
RaeganBridges <rbridge9@vols.utk.edu>
2026-03-29 23:00:56 -0400

    test

---
+++ script.js
@@ -0,0 +1,31 @@
+$(function () {
+    var $exploreToggle = $("#explore-toggle");
+    var $container = $("#container");
+
+    if ($exploreToggle.length && $container.length) {
+        $container.hide();
+        $exploreToggle.on("click", function () {
+            var $btn = $(this);
+            if ($container.is(":visible")) {
+                $container.slideUp(200);
+                $btn.text("Do you want to explore?").attr("aria-expanded", "false");
+            } else {
+                $container.slideDown(200);
+                $btn.text("Hide the list").attr("aria-expanded", "true");
+            }
+        });
+    }
+
+    $(".link").on("click", function () {
+        var $link = $(this);
+        $link.addClass("is-pressed");
+        window.setTimeout(function () {
+            $link.removeClass("is-pressed");
+        }, 180);
+    });
+
+    var $pageContent = $(".page-content");
+    if ($pageContent.length) {
+        $pageContent.hide().fadeIn(350);
+    }
+});
